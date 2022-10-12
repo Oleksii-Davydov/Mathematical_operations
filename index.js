6d368cbaff1274d1ec4df6a5ab811416cf3e148c
Oleksii-Davydov <davydovalekcey1992@gmail.com>
2022-10-12 21:18:37 +0300

    v1

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const a =prompt("Enter the first number");
+const b =prompt("Enter the second number");
+const c1 = (Number(a)+Number(b))
+const c2 = (Number(a)-Number(b))
+const c3 = (Number(a)*Number(b))
+const c4 = (Number(a)/Number(b))
+alert("a+b="+c1+";" + "a-b="+c2+";"+"a*b="+c3+";"+"a/b="+c4+";")
