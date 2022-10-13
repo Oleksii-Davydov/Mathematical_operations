a9ce701fd49e4d040730482a0b7d4b85d2c77eef
Oleksii-Davydov <davydovalekcey1992@gmail.com>
2022-10-13 12:38:49 +0300

    add \n

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
-const a =prompt("Enter the first number");
-const b =prompt("Enter the second number");
+const a =prompt("Enter the first number a");
+const b =prompt("Enter the second number b");
 const c1 = (Number(a)+Number(b))
 const c2 = (Number(a)-Number(b))
 const c3 = (Number(a)*Number(b))
 const c4 = (Number(a)/Number(b))
-alert("a+b="+c1+";" + "a-b="+c2+";"+"a*b="+c3+";"+"a/b="+c4+";")
+alert(a+'+'+b+'='+c1+';'+"\n" + a+'-'+b+'='+c2+';'+"\n"+a+'*'+b+'='+c3+';'+"\n"+a+'/'+b+'='+c4+';')
